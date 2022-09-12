446cb2807770912381c2b2cf5062215e804d44c3
cavid32 <javidhal@code.edu.az>
2022-09-13 00:51:53 +0400

    user

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Username DAXIL ET");
+                string username = Console.ReadLine();
+                if (username.Length >= 8)
+                {
+                    while (true) {
+                        bool isUpperCaseChar = false;
+                        Console.WriteLine("Password DAXIL ET");
+                        string password = Console.ReadLine();
+                        if (password.Length>=8)
+                        {
+                            User user = new User();
+                            user.userName = username;
+                            user.password = password;
+                            Console.WriteLine(user.userName);
+                            Console.WriteLine(user.password);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance
+{
+    class User
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
